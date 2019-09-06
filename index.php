97c343beaf80af5834418f59d7d3488e8fc6c940
Tousten <54995011+Tousten@users.noreply.github.com>
2019-09-06 13:16:34 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<link rel="stylesheet" href="bootstrap431/css/bootstrap.min.css">
+<html>
+<head>
+	<title>Seguro e Funcional - Site de Caronas</title>
+</head>
+<body>
+
+	<form>
+  <div class="form-row">
+    <div class="col-2">
+      <input type="text" class="form-control" placeholder="Origem">
+    </div>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Destino">
+    </div>
+    
+  </div>
+</form>
+
+
+</body>
+</html>

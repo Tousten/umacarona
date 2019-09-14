8a4578460e3a95e40651f76bea6ef6d518299daa
Tousten <54995011+Tousten@users.noreply.github.com>
2019-09-13 21:14:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,25 +3,28 @@
 <link rel="stylesheet" href="bootstrap431/css/bootstrap.min.css">
 <html>
 <head>
-	<title>Seguro e Funcional - Site de Caronas</title>
+	<title>Totalmente Seguro e Funcional - Site de Caronas</title>
 </head>
 <body>
-
-	<form>
-	
-		<div class="mx-auto w-auto">
-			<div class="form-row">
-				<div class="col-2">
-					<input type="text" class="form-control" placeholder="Origem">
-				</div>
-				<div class="col">
-					<input type="text" class="form-control" placeholder="Destino">
-				</div>
-			</div>
-		</div>
-
-	</form>
-
-
+  
+    <div class="mx-auto w-auto">
+	<div class=jumbotron text-center";>
+<form >
+  <div class="form-row" >
+    <div class="col-4">
+      <input type="text" class="form-control" placeholder="Origem">
+    </div>
+    <div class="col-4">
+      <input type="text" class="form-control" placeholder="Destino">
+    </div>
+    <div class="col-2">
+      <input type="text" class="form-control"   placeholder="Saida (opcional)" >
+                
+            </div>
+  </div>
+</form>
+</div>	
+	</div>
+  </div>
 </body>
 </html>

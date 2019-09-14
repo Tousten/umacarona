d913255517f24094b31287214225dcaa596f4503
Tousten <54995011+Tousten@users.noreply.github.com>
2019-09-13 22:07:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,37 @@
 <!DOCTYPE html>
 <meta charset="utf-8">
 <link rel="stylesheet" href="bootstrap431/css/bootstrap.min.css">
+<link rel="stylesheet" href="bootstrap431/css/bootstrap.min.css">
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <html style="height: 100%">
 <head>
-	<title>Totalmente Seguro e Funcional - Site de Caronas</title>
+  <title>Totalmente Seguro e Funcional - Site de Caronas</title>
 </head>
 
 <body style="height: 100%">
-	
-	<div class="container" style="height: 100%">
-		<div class="row" style="height: 100%">
-			<div class="mx-auto align-self-center">
-				<form>
-					<div class="form-row">
-						<div class="col">
-							<input type="text" class="form-control" placeholder="Origem">
-						</div>
-						<div class="col">
-							<input type="text" class="form-control" placeholder="Destino">
-						</div>
-						<div class="col">
-							<input type="text" class="form-control"   placeholder="Saida (opcional)" >
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	
+  
+  <div class="container" style="height: 100%">
+    <div class="row" style="height: 100%">
+      <div class="mx-auto align-self-center">
+        <form>
+          <div class="form-row">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Origem">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Destino">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control"   placeholder="Saida (opcional)" >
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  
 </body>
 </html>

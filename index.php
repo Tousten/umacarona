95b0f98ae8f1e8b28458e852693aeb1ca57fd4d1
Cristiano M Bergamo <cmbergamo@outlook.com>
2019-09-13 21:39:34 -0300

    Centralização vestical/horizontal

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <!DOCTYPE html>
 <meta charset="utf-8">
 <link rel="stylesheet" href="bootstrap431/css/bootstrap.min.css">
-<html>
+<html style="height: 100%">
 <head>
 	<title>Seguro e Funcional - Site de Caronas</title>
 </head>
-<body>
-
-	<form>
+<body style="height: 100%">
 	
-		<div class="mx-auto w-auto">
-			<div class="form-row">
-				<div class="col-2">
-					<input type="text" class="form-control" placeholder="Origem">
-				</div>
-				<div class="col">
-					<input type="text" class="form-control" placeholder="Destino">
+		<div class="container" style="height: 100%">
+			<div class="row" style="height: 100%">
+				<div class="mx-auto align-self-center">
+					<form>
+						<div class="form-row">
+							<div class="col">
+								<input type="text" class="form-control" placeholder="Origem">
+							</div>
+							<div class="col">
+								<input type="text" class="form-control" placeholder="Destino">
+							</div>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
-
-	</form>
-
+	
 
 </body>
 </html>
